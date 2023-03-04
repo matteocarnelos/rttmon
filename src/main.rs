f5ab1fb35d91bcfd73bf6678b87f179aba2aa154
RealIndrit <johan.indrit.andersson@gmail.com>
2023-03-04 15:55:47 +0100

    Added option to dump RTT data to file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::fs::OpenOptions;
+use std::io::prelude::*;
 use std::io::{BufRead, BufReader, ErrorKind};
 use std::net::TcpStream;
 use std::process::exit;
@@ -19,12 +21,37 @@ struct Args {
     /// The OpenOCD RTT server port
     #[arg(default_value_t = 9090)]
     port: u16,
+
+    /// Dump target for OpenOCD RTT data
+    #[arg(default_value = "")]
+    file: String,
+}
+
+
+/// Format data string to have Local.now timestamp
+fn format_data(data:String) -> std::string::String {
+    let now = Local::now().format("%T%.3f");
+    let formatted_string = format!("{} {}", format!("[{}]", now).bright_black(), data);
+    return formatted_string;
+}
+
+fn append_file(path: String, data:String) {
+    let mut file = OpenOptions::new()
+        .create(true)
+        .append(true) // append(true) implies write(true)
+        .open(path)
+        .unwrap();
+
+    if let Err(e) = writeln!(file, "{}", data) {
+        eprintln!("Couldn't write to file: {}", e);
+    }
 }
 
 fn main() {
     let args = Args::parse();
     let address = format!("{}:{}", args.host, args.port);
     let mut waiting = false;
+
     loop {
         sleep(Duration::from_secs(1));
         let stream = TcpStream::connect(&address);
@@ -60,8 +87,11 @@ fn main() {
                     if c == 0 {
                         break;
                     }
-                    let now = Local::now().format("%T%.3f");
-                    print!("{} {}", format!("[{}]", now).bright_black(), line);
+                    let formatted_line = format_data(line);
+                    if args.file.len() != 0 {
+                        append_file(args.file.clone(), formatted_line.clone());
+                    }
+                    println!("{}", formatted_line);
                 }
                 Err(_) => {
                     break;
